a572705f402aa39e82bd91907dd7f8d8fee7c469
wyao <289672494@qq.com>
2019-09-06 14:55:43 +0800

    修改祝福语

--- config/service_config.py
+++ config/service_config.py
@@ -5,7 +5,7 @@ EMAIL_HOST = 'smtp.qq.com'
 EMAIL_TITLE = 'System'
 EMAIL_CONT = '    一位朋友将你设为暗恋的对象， 如果你也有暗恋的对象，说不定Ta就是你心中的Ta哦' \
              '， 进入 http://123.207.27.133:50070/confession/ 可以向你心中的Ta发送告白信息尝试配对'
-EMAIL_CONT_CONGRATULATE = '    你喜欢的人也喜欢你呢'
+EMAIL_CONT_CONGRATULATE = '    恭喜你， 你所喜欢的Ta同样默默喜欢着你。\n    祝你一生幸福，两情相悦，三生缘修，世世代代，无离无弃，流年大喜，把手言老，酒杯遥祝，十全十美，百子千孙，万事如意。'
 
 
 EMAIL_CONFESSION = '0'
